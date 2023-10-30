5bcff6aea98018be2a8367b77744b0ffbf51c3ae
DaybreakStudiosOfficial <149344556+DaybreakStudiosOfficial@users.noreply.github.com>
2023-10-29 22:56:43 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: 'export',
+  images: {
+    loader: 'custom',
+    loaderFile: './my-loader.ts',
+  }
 }
 
 module.exports = nextConfig
